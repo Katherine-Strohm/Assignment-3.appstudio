808e68d0ebb1476ed80d721bbdef5c1a99a373f5
Katherine-Strohm <90561982+Katherine-Strohm@users.noreply.github.com>
2021-10-03 23:40:00 -0500

    Update dogStore.js

--- forms/dogStore/dogStore.js
+++ forms/dogStore/dogStore.js
@@ -1,2 +1,7 @@
-let items = ["Crocheted Dog Toy","Slow Feeder Bowl","Pawz Grey Treat Jar","Blue Sweet Dreams Thermal Pajamas","Checker Chewy Vuiton Bowl","Red Heart Sweater Dress","Velvet Hair Bow - Sun Kissed","Into the Wild Leash"]
-let prices = ["10.00", "$14.00","$20.00","$30.00","32.00","$27.00","$12.00","$27.00"]
+name = []
+
+let products = ["Crocheted Dog Toy", "Slow Feeder Bowl", "Pawz Grey Treat Jar", "Blue Sweet Dreams Thermal Pajamas", "Checker Chewy Vuiton Bowl", "Red Heart Sweater Dress", "Velvet Hair Bow - Sun Kissed", "Into the Wild Leash"]
+
+let productPrice = ["$10.00", "$14.00", "$20.00", "$30.00", "$32.00", "$27.00", "$12.00", "$27.00"]
+
+let wishList = []
