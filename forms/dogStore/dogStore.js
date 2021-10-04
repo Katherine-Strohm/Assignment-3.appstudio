eb49e4c5ebd029cf600f6db954894c6347d11c06
Katherine-Strohm <90561982+Katherine-Strohm@users.noreply.github.com>
2021-10-03 20:08:05 -0500

    dog

---
+++ forms/dogStore/dogStore.js
@@ -0,0 +1,2 @@
+let items = ["Crocheted Dog Toy","Slow Feeder Bowl","Pawz Grey Treat Jar","Blue Sweet Dreams Thermal Pajamas","Checker Chewy Vuiton Bowl","Red Heart Sweater Dress","Velvet Hair Bow - Sun Kissed","Into the Wild Leash"]
+let prices = ["10.00", "$14.00","$20.00","$30.00","32.00","$27.00","$12.00","$27.00"]
---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,8 @@
+let pass1=""
+let pass2=""
+function Compare(pass1, pass2) {
+  let funcCompare = pass1 = pass2
+  return funcCompare
+}
+let answer = ("The passwords are the same")
+console.log(`${answer}`)
